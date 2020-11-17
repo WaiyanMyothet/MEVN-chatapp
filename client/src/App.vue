7bdced1feebc9bea2a5933cb613011585dee232f
Waiyan Myothet <menooby1996@gmail.com>
2020-11-17 10:04:23 +0630

    added basice views

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="app">
+        <Navbar/>
+        <Particle name="particle-js"/>
+        <transition
+            :name="transitionName"
+            :enter-active-class="enterActive"
+            :leave-active-class="leaveActive"
+            mode="out-in"
+        >
+            <router-view/>
+        </transition>
+        <Footer v-if="['Home', 'Login', 'Register', 'About'].includes($route.name)"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import _ from 'lodash';
+
+import Navbar from '@/components/layout/Navbar.vue';
+import Footer from '@/components/layout/Footer.vue';
+import Particle from '@/components/layout/Particle.vue';
+
+const DEFAULT_TRANSITION = 'fade';
+const DEFAULT_ENTER_ACTIVE_CLASS = 'animated fadeIn';
+const DEFAULT_LEAVE_ACTIVE_CLASS = 'animated fadeOut';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: {
+        Navbar: Navbar,
+        Particle,
+        Footer
+    },
+    data: function() {
+        return {
+            transitionName: DEFAULT_TRANSITION,
+            leaveActive: DEFAULT_LEAVE_ACTIVE_CLASS,
+            enterActive: DEFAULT_ENTER_ACTIVE_CLASS
+        };
+    },
+    methods: {
+        resetTransition() {
+            this.transitionName = DEFAULT_TRANSITION;
+            this.enterActive = DEFAULT_ENTER_ACTIVE_CLASS;
+            this.leaveActive = DEFAULT_LEAVE_ACTIVE_CLASS;
+        }
+    },
+    created() {
+        this.$router.beforeEach((to, from, next) => {
+            if (!_.isEmpty(to.meta)) {
+                if (to.meta.transitionName) {
+                    this.transitionName = to.meta.transitionName;
+                }
+
+                if (to.meta.enterActive) {
+                    this.enterActive = to.meta.enterActive;
+                }
+
+                if (to.meta.leaveActive) {
+                    this.leaveActive = to.meta.leaveActive;
+                }
+            }
+
+            if (!to.meta.requiresAuth) {
+                this.resetTransition();
+            }
+            next();
+        });
+    }
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import '@/assets/scss/base/base.scss';
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,50 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router';
+import store from './store';
+import setAuthToken from './utils/authToken';
+import axios from 'axios';
 
 Vue.config.productionTip = false
+Vue.config.ignoredElements = ['ion-icons', /^ion-/];
+
+/** Check for auth token on refresh and set authorization header for incoming requests */
+if (localStorage.authToken) {
+  setAuthToken(localStorage.authToken);
+} else {
+  setAuthToken(null);
+}
+
+/** Axios Request Intercept */
+axios.interceptors.request.use(
+  function(config) {
+      return config;
+  },
+  function(err) {
+      return Promise.reject(err);
+  }
+);
+
+/** Axios Response Intercept */
+axios.interceptors.response.use(
+  function(response) {
+      return response;
+  },
+  function(err) {
+      if (err.response.status === 401) {
+          localStorage.removeItem('authToken');
+          store.dispatch('toggleAuthState', false);
+          router.push({
+              name: 'Login',
+              params: { message: 'Session has expired, please login again' }
+          });
+      }
+      return Promise.reject(err);
+  }
+);
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
